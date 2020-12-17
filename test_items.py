a1c974b5c6a099f47937cc4a41a2efabc37fc329
Denis Chilyakov <chilyakov@gmail.com>
2020-12-17 21:57:13 +0500

    added the lesson 3.6 (step 9) files

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_existence_button_add_to_basket(browser):
+    browser.get(link)
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert button is not None, "Button not found!"
+
+    time.sleep(15)
